505e30d2c67eb7714f6f7ddd3b078919e54f7aef
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-13 16:33:25 -0300

    setup eslint para importação arquivos .ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is 🏃 on 🚪 3333'));
