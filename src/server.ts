e2735e05312d77784151df74dc311c6dcb414ef7
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-16 01:55:44 -0300

    Implementação middleware erro global

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
+import AppError from './errors/AppError';
 import uploadConfig from './config/upload';
 
 import routes from './routes';
@@ -9,9 +10,28 @@ import routes from './routes';
 import './database';
 
 const app = express();
+
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => console.log('Server is 🏃 on 🚪 3333'));
