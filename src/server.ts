8cc13d12e422b1e39beb30b5d186a10dc9b64440
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-16 01:28:11 -0300

    Servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import 'dotenv/config';
 
 import express from 'express';
 
+import uploadConfig from './config/upload';
+
 import routes from './routes';
 
 import './database';
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
