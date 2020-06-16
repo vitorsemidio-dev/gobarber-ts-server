2f27a5c5c0f43d319b8901f11f07c7040ba2073f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-16 01:58:21 -0300

    Instalação express-async-errors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import AppError from './errors/AppError';
 import uploadConfig from './config/upload';
