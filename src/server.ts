8b729a6ce53d52eaef9bfb88223655e386d77f0f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-16 02:03:06 -0300

    eslint @typescript-eslint/no-unused-vars

--- src/server.ts
+++ src/server.ts
@@ -17,22 +17,20 @@ app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-
-    console.error(err);
-
-    return response.status(500).json({
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
       status: 'error',
-      message: 'Internal server error',
+      message: err.message,
     });
-  },
-);
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => console.log('Server is 🏃 on 🚪 3333'));
