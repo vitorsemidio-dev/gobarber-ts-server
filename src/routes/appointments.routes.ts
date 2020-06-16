a57a58584b1934ce9aa174b775f2b33d189cb7ae
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-16 01:49:46 -0300

    Remoção try catch

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -14,22 +14,18 @@ const appointmentsRouter = Router();
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointmentService = new CreateAppointmentService();
+  const createAppointmentService = new CreateAppointmentService();
 
-    const appointment = await createAppointmentService.execute({
-      provider_id,
-      date: parsedDate,
-    });
+  const appointment = await createAppointmentService.execute({
+    provider_id,
+    date: parsedDate,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ message: err.message });
-  }
+  return response.json(appointment);
 });
 
 appointmentsRouter.get('/', async (request, response) => {
