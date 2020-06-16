ed40e9d8d1709ef929c33e8b9cd191afc26ca90d
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-15 22:34:29 -0300

    Gerando token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,11 +11,19 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
   public async execute({ email, password }: Request): Promise<Response> {
     const usersRepository = getRepository(User);
+
+    const secretMd5 = process.env.MD5_HASH;
+
+    // TODO: Remove
+    if (!secretMd5) {
+      throw new Error('Secret is missing');
+    }
 
     const user = await usersRepository.findOne({
       where: { email },
@@ -30,8 +39,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, secretMd5, {
+      subject: user.id,
+      expiresIn: '1h',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
