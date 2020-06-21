da2bc1389bb1db70a427906540c079023cc43878
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-21 13:27:55 -0300

    Instalação cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import AppError from './errors/AppError';
@@ -12,6 +13,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 
