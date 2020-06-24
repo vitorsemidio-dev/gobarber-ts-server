b4909ef6c740543e60254e956e13e856adb76e6a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-24 02:00:08 -0300

    Ajustes importação

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,12 +4,12 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import AppError from './errors/AppError';
-import uploadConfig from './config/upload';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
 import routes from './routes';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
