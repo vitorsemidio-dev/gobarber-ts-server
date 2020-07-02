158e2615b740f520b55284f42036ec3d6e19b5aa
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-02 18:19:25 -0300

    Remoção relação typeorm

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,26 +1,14 @@
-import { getRepository, Repository } from 'typeorm';
-
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import User from '../entities/User';
 
 class UsersRepository implements IUsersRepository {
-  private ormRepository: Repository<User>;
-
-  constructor() {
-    this.ormRepository = getRepository(User);
-  }
-
   public async findById(id: string): Promise<User | undefined> {
-    const user = await this.ormRepository.findOne(id);
-
     return user;
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const user = await this.ormRepository.findOne({ where: { email } });
-
     return user;
   }
 
@@ -29,18 +17,10 @@ class UsersRepository implements IUsersRepository {
     email,
     password,
   }: ICreateUserDTO): Promise<User> {
-    const user = this.ormRepository.create({
-      name,
-      email,
-      password,
-    });
-
     return user;
   }
 
-  public async save(user: User): Promise<User> {
-    return this.ormRepository.save(user);
-  }
+  public async save(user: User): Promise<User> {}
 }
 
 export default UsersRepository;
