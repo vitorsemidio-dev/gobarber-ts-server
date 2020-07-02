cc2b7a1cc738aae8956ec0cf905fa497ec593989
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-02 18:11:40 -0300

    eslint test create appointment service

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -7,7 +7,9 @@ describe('Create Appointment', () => {
   it('should be able to create a new appointment', async () => {
     const fakeAppointmentsRepository = new FakeAppointmentsRepository();
 
-    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
     const appointment = await createAppointment.execute({
       date: new Date(),
@@ -21,7 +23,9 @@ describe('Create Appointment', () => {
   it('should not be able create two appointments on the same time', async () => {
     const fakeAppointmentsRepository = new FakeAppointmentsRepository();
 
-    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
     const appointmentDate = new Date();
 
