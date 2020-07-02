bfc54e8de3333ad9b2cd6b7a7aeddbd6076c1e46
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-02 18:25:15 -0300

    Implementação métodos FakeUsersRepository

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,15 +1,21 @@
+import { uuid } from 'uuidv4';
+
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
-import User from '../entities/User';
+import User from '../../infra/typeorm/entities/User';
 
 class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
   public async findById(id: string): Promise<User | undefined> {
-    return user;
+    const findUser = this.users.find(user => user.id === id);
+    return findUser;
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    return user;
+    const findUser = this.users.find(user => user.email === email);
+    return findUser;
   }
 
   public async create({
@@ -17,10 +23,22 @@ class UsersRepository implements IUsersRepository {
     email,
     password,
   }: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid(), name, email, password });
+
+    this.users.push(user);
+
     return user;
   }
 
-  public async save(user: User): Promise<User> {}
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
 }
 
 export default UsersRepository;
