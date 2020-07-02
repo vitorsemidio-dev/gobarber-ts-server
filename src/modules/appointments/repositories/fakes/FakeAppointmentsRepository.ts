a9c88bd8f052b91727fd4dd7bd5d5401a95cd6cb
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-02 18:10:47 -0300

    Fix: eslint

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -10,7 +10,9 @@ class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(appointment => isEqual(appointment.date, date));
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
+    );
 
     return findAppointment;
   }
