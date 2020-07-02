11f1f8814bbc0d016d2b6b1e8fe60d91e9e77d10
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-02 15:16:33 -0300

    Descrição com it nos testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,11 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('Create Appointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it('should not be able create two appointments on the same time', () => {
+    expect(1 + 2).toBe(3);
+  });
 });
