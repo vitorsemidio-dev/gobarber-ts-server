9a9499627077a36dfca107b2e2e0c827811ae8b6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-02 21:19:12 -0300

    Injetando fakeHashProvider nos teste em createUser

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,15 +1,20 @@
 import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
 
 describe('AuthenticateUser', () => {
   it('should be able to authenticate', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
 
     const authenticateUser = new AuthenticateUserService(fakeUsersRepository);
-    const createUserService = new CreateUserService(fakeUsersRepository);
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     await createUserService.execute({
       name: 'John Doe',
