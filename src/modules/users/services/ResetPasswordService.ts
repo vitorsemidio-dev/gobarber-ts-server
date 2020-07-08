24195283c43c7a159a65277f879b0cfa6018878b
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-08 12:05:58 -0300

    Criação Serviço e teste ResetPassword

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,23 @@
+import { inject, injectable } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+interface IRequest {
+  password: string;
+  token: string;
+}
+
+@injectable()
+class ResetPasswordService {
+  constructor(
+    @inject('UsersRepository') private usersRepository: IUsersRepository,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ password, token }: IRequest): Promise<void> {}
+}
+
+export default ResetPasswordService;
