f304fc654d7815775d444f834b32c432352b6aaf
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-08 11:27:11 -0300

    Implementação geração token em forgotPassword

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,7 @@ import { inject, injectable } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
 
 // import User from '../infra/typeorm/entities/User';
 
@@ -15,14 +16,19 @@ class SendForgotPasswordEmailService {
   constructor(
     @inject('UsersRepository') private usersRepository: IUsersRepository,
     @inject('MailProvider') private mailRepository: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    const checkUserExists = await this.usersRepository.findByEmail(email);
+    const user = await this.usersRepository.findByEmail(email);
 
-    if (!checkUserExists) {
+    if (!user) {
       throw new AppError('User does not exist');
     }
+
+    await this.userTokensRepository.generate(user.id);
+
     this.mailRepository.sendMail(
       email,
       'Pedido de Recuperação de senha recebido',
