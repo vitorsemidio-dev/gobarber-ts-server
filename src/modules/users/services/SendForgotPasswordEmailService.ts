75ba34877506f5ab1d7a12e45bab8ef77ff21102
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-08 10:15:45 -0300

    Implementação checkUserExists

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -18,6 +18,11 @@ class SendForgotPasswordEmailService {
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (!checkUserExists) {
+      throw new AppError('User does not exist');
+    }
     this.mailRepository.sendMail(
       email,
       'Pedido de Recuperação de senha recebido',
