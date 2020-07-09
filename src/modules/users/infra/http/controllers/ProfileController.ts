c1a28112bffa8f2b4762968e739d0a1c5c172da1
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-09 12:54:14 -0300

    ShowProfileService e Route

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -2,12 +2,17 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
 
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
-    // show profile
+    const user_id = request.user.id;
 
-    return response.json();
+    const showProfile = container.resolve(ShowProfileService);
+
+    const user = await showProfile.execute({ user_id });
+
+    return response.json(user);
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
