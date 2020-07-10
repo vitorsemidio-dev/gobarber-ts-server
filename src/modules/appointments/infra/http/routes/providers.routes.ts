02468f5bba6c3dc6f18dd664b9ab64284b7f4620
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-10 15:26:33 -0300

    Controllers e Routes para listagem de disponibilidade

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
 
 const providersRoute = Router();
 const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 providersRoute.use(ensureAuthenticated);
 
 providersRoute.get('/', providersController.index);
+
+providersRoute.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
+providersRoute.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
 
 export default providersRoute;
