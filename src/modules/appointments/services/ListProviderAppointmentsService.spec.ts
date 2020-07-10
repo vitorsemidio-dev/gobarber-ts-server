7f1872fc4e90e2fb82cd43bd9fa3581f1ab948f6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-10 15:57:11 -0300

    Listagem dos agendamentos do dia do prestador

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+let listProviderAppointments: ListProviderAppointmentsService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderAppointmentsService', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments from a provider on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+      date: new Date(2020, 9, 1, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider_id',
+      user_id: 'user_id',
+      date: new Date(2020, 9, 1, 15, 0, 0),
+    });
+
+    const appointmentsInTheDay = await listProviderAppointments.execute({
+      provider_id: 'provider_id',
+      day: 1,
+      month: 10,
+      year: 2020,
+    });
+
+    expect(appointmentsInTheDay).toEqual([appointment1, appointment2]);
+  });
+});
