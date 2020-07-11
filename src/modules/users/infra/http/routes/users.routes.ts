373e86ecb04c467ff067f924881bae1bbfa5adbe
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-10 22:32:50 -0300

    Validação Rotas

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import uploadConfig from '@config/upload';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
@@ -11,7 +13,17 @@ const upload = multer(uploadConfig);
 const userAvatarController = new UserAvatarController();
 const usersController = new UsersController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
